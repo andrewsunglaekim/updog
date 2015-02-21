db0431dedab2ad62376e3fed45b2b91a272d6bbc
Jesse Shawl <jesse@jshawl.com>
2015-02-21 07:34:27 -0500

    rescue file not found error

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -11,13 +11,18 @@ class SitesController < ApplicationController
   end
   def load
     @site = Site.find_by(name: request.subdomain)
-    @content = @site.content get_client, request.env['PATH_INFO']
+    begin
+      @content = @site.content get_client, request.env['PATH_INFO']
+    rescue Exception => err
+      @content = err
+    end
     respond_to do |format|
       format.all { render :html => @content, :layout => false }
     end
   end
   def create
     @site = Site.new site_params.merge( user_id: session[:user_id] )
+    @site.name.downcase!
     @site.domain = @site.name + '.updog.co'
     @db = get_client
     if @site.save
