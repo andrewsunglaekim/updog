0f1e0cc27d019e9763c7aa1a48022f797f97eede
Jesse Shawl <jesse@jshawl.com>
2015-02-24 07:12:14 -0500

    add pretty urls

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy'
   get '/auth/dropbox', to: 'sessions#new'
   match '/*req', to: 'sites#load', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
-  resources :sites
   get '/about', to: 'pages#about'
   get '/source', to: 'pages#source'
   get '/pricing', to: 'pages#pricing'
   get '/admin', to: 'pages#admin'
   get '/webhook', to: 'webhook#challenge'
   post '/webhook', to: 'webhook#post'
+  resources :sites, path: '' do
+
+  end
 end
